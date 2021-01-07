3068b5d37115b26abff87aaa4291bc300c2aa832
Pedro Bruno <55460469+pedro-bX1@users.noreply.github.com>
2021-01-07 18:15:22 -0300

    upload do código

---
+++ codigo.py
@@ -0,0 +1,17 @@
+primeiro_numero = float(input('digite o primeiro numero'))
+segundo_numero = float(input('digite o segundo numero'))
+
+soma = primeiro_numero + segundo_numero
+subtracao = primeiro_numero - segundo_numero
+divisao = primeiro_numero / segundo_numero
+multiplicacao = primeiro_numero * segundo_numero
+
+print(f' a soma entre { primeiro_numero } e { segundo_numero } é { soma }')
+print(f' a divisão entre { primeiro_numero } e { segundo_numero } é {divisao }')
+print(f' a subtração entre { primeiro_numero} e { segundo_numero } é { subtracao }')
+print(f' a multiplicação entre { primeiro_numero } e { segundo_numero } é { multiplicacao }')
+
+print('a soma é', soma)
+print('a divisao é', divisao)
+print('a multiplicação é', multiplicacao)
+print('a subtração é', subtracao)
